c29a801a32196e12e307e766311b5b3b1d018ec1
ChobitsTheZero <Emil.Nordlander@hotmail.com>
2018-10-17 16:33:25 +0200

    Update QueueingModel/ClosedQueue.cpp

--- QueueingModel/ClosedQueue.cpp
+++ QueueingModel/ClosedQueue.cpp
@@ -1,109 +1,171 @@
-//======================================================== file = mva.c =====
-//=  Mean Value Analysis program for closed queueing networks               =
-//===========================================================================
-//=  Notes:                                                                 =
-//=    1)  Solves for a closed network of tandem queues (i.e., all          =
-//=        visit ratios are 1.0)                                            =
-//=    2)  Must set M (number of nodes) and N (number of customers)         =
-//=        as constant values and service rate (mu[]) for each node         =
-//=    3)  Nodes are labled as 1, 2, ... M                                  =
-//=    4)  Output is to stdout                                              =
-//=    5)  The MVA algorithm is based on the Mean Value Theorem,            =
-//=          "For closed queueing networks of the exponential type, the     =
-//=           state of the system at the time of arrival of a customer has  =
-//=           a probability distribution equal to that of steady-state      =
-//=           distribution of the network with one customer less."          =
-//=-------------------------------------------------------------------------=
-//= Example output (for M = 3, N = 2, mu[1] = mu[2] = mu[3] = 1.0):         =
-//=                                                                         =
-//=   1 customers --- thruput = 0.333333 cust/sec and delay = 3.000000 sec  =
-//=         >>> n[1] = 0.333333 --- t[1] = 1.000000                         =
-//=         >>> n[2] = 0.333333 --- t[2] = 1.000000                         =
-//=         >>> n[3] = 0.333333 --- t[3] = 1.000000                         =
-//=   2 customers --- thruput = 0.500000 cust/sec and delay = 4.000000 sec  =
-//=         >>> n[1] = 0.666667 --- t[1] = 1.333333                         =
-//=         >>> n[2] = 0.666667 --- t[2] = 1.333333                         =
-//=         >>> n[3] = 0.666667 --- t[3] = 1.333333                         =
-//=-------------------------------------------------------------------------=
-//=  Build:  bcc32 mva.c, cl mva.c, gcc mva.c                               =
-//=-------------------------------------------------------------------------=
-//=  History:  KJC (01/11/98) - Genesis (adapted from Molloy, 1989)         =
-//===========================================================================
-
-//----- Include files -------------------------------------------------------
+//================================================== ========= file = mm1.c =====
+//= A simple "straight C" M/M/1 queue simulation =
+//================================================== ============================
+//= Notes: =
+//= 1) This program is adapted from Figure 1.6 in Simulating Computer =
+//= Systems, Techniqyes and Tools by M. H. MacDougall (1987). =
+//= 2) The values of SIM_TIME, ARR_TIME, and SERV_TIME need to be set. =
+//=----------------------------------------------------------------------------=
+//= Build: gcc mm1.c -lm, bcc32 mm1.c, cl mm1.c =
+//=----------------------------------------------------------------------------=
+//= Execute: mm1 =
+//=----------------------------------------------------------------------------=
+//= History: KJC (03/09/99) - Genesis =
+//= KJC (05/23/09) - Added RNG function (so not to use compiler RNG) =
+//= KJC (05/24/09) - Added updated of busy time at end of main loop =
+//================================================== ============================
+//----- Include files ----------------------------------------------------------
+#include <stdio.h> // Needed for printf()
+#include <stdlib.h> // Needed for exit() and rand()
+#include <math.h> // Needed for log()
 #include <iostream>
-#include <stdio.h>   // Needed for printf()
 
-using namespace std;
+//----- Constants --------------------------------------------------------------
+#define SIM_TIME 1.0e6 // Simulation time
+#define ARR_TIME 1.25 // Mean time between arrivals
+#define SERV_TIME 1.00 // Mean service time
 
-//----- Defines -------------------------------------------------------------
-#define M   4        // Number of nodes
-#define N   5        // Number of customers
+//----- Function prototypes ----------------------------------------------------
+double rand_val(int seed); // RNG for unif(0,1)
+double exponential(double x); // Generate exponential RV with mean x
 
-//===========================================================================
-//=  Main program                                                           =
-//===========================================================================
-int main()
+//===== Main program ================================================== =========
+int main(void)
 {
-	float ar = 0.0394;		// service demand
-	double Z = 2.0;         // think time
-	float mu[M + 1];     // mu[i] = service rate for each node
-	int   count;       // Current number of customers in network
-	float t[M + 1];      // t[i] = delay at node i w/ count customers in network
-	float s[N + 1];      // s[count] = thruput with count customers in network
-	float n[M + 1];      // n[i] = num at node i w/ count customers in network
-	float total_delay; // Current total delay across all nodes (sum of n[i])
-	int   i;           // Counter variable
+	double end_time = SIM_TIME; // Total time to simulate
+	double Ta = ARR_TIME; // Mean time between arrivals
+	double Ts = SERV_TIME; // Mean service time
+	double time = 0.0; // Simulation time
+	double t1 = 0.0; // Time for next event #1 (arrival)
+	double t2 = SIM_TIME; // Time for next event #2 (departure)
+	unsigned int n = 0; // Number of customers in the system
+	unsigned int c = 0; // Number of service completions
+	double b = 0.0; // Total busy time
+	double s = 0.0; // Area of number of customers in system
+	double tn = time; // Variable for "last event time"
+	double tb; // Variable for "last start of busy time"
+	double x; // Throughput
+	double u; // Utilization
+	double l; // Mean number in the system
+	double w; // Mean residence time
 
-	double Qi[100];
-	double Rprime[100];
-	double X[100];
+	// Seed the RNG
+	rand_val(1);
 
-	// Initialize service rate (cust/sec) for all M nodes
-	mu[1] = 0.0771;
-	mu[2] = 0.1238;
-	mu[3] = 0.0804;
-	mu[4] = 0.235;
-
-	// Initialize the number of customers in all M nodes to zero
-	for (i = 1; i <= M; i++)
-		n[i] = 0.0;
-
-	// Main loop to iterate for customer count from 1 to N
-	for (count = 1; count <= N; count++)
+	// Main simulation loop
+	while (time < end_time)
 	{
-
-		Rprime[count] = ar * (1.0 + Qi[count - 1]);
-		X[count] = count / (Z + Rprime[count]);
-		Qi[count] = X[count] * Rprime[count];
-
-		// Calculate the average waiting time in each node with an
-		// additional customer based on the number of customers in each
-		// queue from the previous loop.  This is the "PASTA step".
-		for (i = 1; i <= M; i++)
-			t[i] = (1.0 / mu[i]) * (n[i] + 1.0);     // <--- Little's Law
-
-		  // Calculate the average throughput for the network
-		total_delay = 0.0;
-		for (i = 1; i <= M; i++)
-			total_delay = total_delay + t[i];
-		s[count] = count / total_delay;            // <--- Little's Law
-
-		// Calculate the average number of customers in each queue
-		for (i = 1; i <= M; i++)
-			n[i] = s[count] * t[i];                    // <--- Little's Law
-
-		  // Output network statistics (customer count, throughput, and delay)
-		printf("%d customers --- thruput = %f cust/sec and delay = %f sec \n",
-			count, s[count], total_delay);
-
-		// Output node statistics (length and delay)
-		for (i = 1; i <= M; i++)
-			printf("    >>> n[%d] = %f --- t[%d] = %f \n", i, n[i], i, t[i]);
+		if (t1 < t2) // *** Event #1 (arrival) ***
+		{
+			time = t1;
+			s = s + n * (time - tn); // Update area under "s" curve
+			n++;
+			tn = time; // tn = "last event time" for next event
+			t1 = time + exponential(Ta);
+			if (n == 1)
+			{
+				tb = time; // Set "last start of busy time"
+				t2 = time + exponential(Ts);
+			}
+		}
+		else // *** Event #2 (departure) ***
+		{
+			time = t2;
+			s = s + n * (time - tn); // Update area under "s" curve
+			n--;
+			tn = time; // tn = "last event time" for next event
+			c++; // Increment number of completions
+			if (n > 0)
+				t2 = time + exponential(Ts);
+			else
+			{
+				t2 = SIM_TIME;
+				b = b + time - tb; // Update busy time sum if empty
+			}
+		}
 	}
 
-	cin.get();
+	// End of simulation so update busy time sum
+	b = b + time - tb;
 
-	return 0;
+	// Compute outputs
+	x = c / time; // Compute throughput rate
+	u = b / time; // Compute server utilization
+	l = s / time; // Compute mean number in system
+	w = l / x; // Compute mean residence or system time
+
+	// Output results
+	printf("========================================== ===================== \n");
+	printf("= *** Results from M/M/1 simulation *** = \n");
+	printf("========================================== ===================== \n");
+	printf("= Total simulated time = %3.4f sec \n", end_time);
+	printf("========================================== ===================== \n");
+	printf("= INPUTS: \n");
+	printf("= Mean time between arrivals = %f sec \n", Ta);
+	printf("= Mean service time = %f sec \n", Ts);
+	printf("========================================== ===================== \n");
+	printf("= OUTPUTS: \n");
+	printf("= Number of completions = %ld cust \n", c);
+	printf("= Throughput rate = %f cust/sec \n", x);
+	printf("= Server utilization = %f %% \n", 100.0 * u);
+	printf("= Mean number in system = %f cust \n", l);
+	printf("= Mean residence time = %f sec \n", w);
+	printf("========================================== ===================== \n");
+
+	std::cin.get();
+
+	return(0);
 }
-
+
+//================================================== =======================
+//= Multiplicative LCG for generating uniform(0.0, 1.0) random numbers =
+//= - x_n = 7^5*x_(n-1)mod(2^31 - 1) =
+//= - With x seeded to 1 the 10000th x value should be 1043618065 =
+//= - From R. Jain, "The Art of Computer Systems Performance Analysis," =
+//= John Wiley & Sons, 1991. (Page 443, Figure 26.2) =
+//= - Seed the RNG if seed > 0, return a unif(0,1) if seed == 0 =
+//================================================== =======================
+double rand_val(int seed)
+{
+	const long a = 16807; // Multiplier
+	const long m = 2147483647; // Modulus
+	const long q = 127773; // m div a
+	const long r = 2836; // m mod a
+	static long x; // Random int value (seed is set to 1)
+	long x_div_q; // x divided by q
+	long x_mod_q; // x modulo q
+	long x_new; // New x value
+
+	// Seed the RNG
+	if (seed != 0) x = seed;
+
+	// RNG using integer arithmetic
+	x_div_q = x / q;
+	x_mod_q = x % q;
+	x_new = (a * x_mod_q) - (r * x_div_q);
+	if (x_new > 0)
+		x = x_new;
+	else
+		x = x_new + m;
+
+	// Return a random value between 0.0 and 1.0
+	return((double)x / m);
+}
+
+//================================================== ============================
+//= Function to generate exponentially distributed RVs using inverse method =
+//= - Input: x (mean value of distribution) =
+//= - Output: Returns with exponential RV =
+//================================================== ============================
+double exponential(double x)
+{
+	double z; // Uniform random number from 0 to 1
+
+	// Pull a uniform RV (0 < z < 1)
+	do
+	{
+		z = rand_val(0);
+	} while ((z == 0) || (z == 1));
+
+	return(-x * log(z));
+}
